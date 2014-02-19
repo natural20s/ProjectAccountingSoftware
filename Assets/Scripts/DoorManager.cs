2b8ef42753c2bbdbdf67ad0748536decab814b88
jordanajlouni <jordanajlouni@gmail.com>
2014-02-19 11:12:37 -0500

    Jordan Asset Extravaganza
    Jordan has some new assets, and they're coming your way!!!

---
+++ Assets/Scripts/DoorManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorManager : MonoBehaviour 
+{
+
+	const string DOOR_OPEN_BOOL = "open";
+	const string DOOR_OPEN_STATE = "DoorOpen";
+	
+	public Animator m_doorAnimator;
+	public Collider2D m_colliderRef;
+
+	public void Start()
+	{
+//		m_openStateHash = Animator.StringToHash("Base." + DOOR_OPEN_STATE); 
+	}
+
+	public void Update()
+	{
+		if(Input.GetKeyDown (KeyCode.Space))
+		{
+			OpenDoor();
+		}
+		else if(Input.GetKeyUp (KeyCode.Space))
+		{
+			CloseDoor();
+		}
+
+		if(m_doorAnimator.GetCurrentAnimatorStateInfo(0).IsName(DOOR_OPEN_STATE))
+		{
+			Debug.Log ("In Open State");
+			m_colliderRef.enabled = false;
+		}
+		else
+		{
+			m_colliderRef.enabled = true;
+		}
+	}
+
+	public void OpenDoor()
+	{
+		m_doorAnimator.SetBool(DOOR_OPEN_BOOL, true);
+	}
+
+	public void CloseDoor()
+	{
+		m_doorAnimator.SetBool(DOOR_OPEN_BOOL, false);
+	}
+}
